8814713ce7150024f5310ec57bf366c412d9e310
michal-mietus <michal.mietus@sternick.pl>
2019-11-23 21:03:55 +0100

    Responsive mobile resume tab.

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ window.onload = () => {
 }
 
 const collapseResumeItem = (clickEvent) => {
+    // TODO extend for mobile buttons
+
     const plusButtonClassName = 'resume__icon--plus-button';
     const minusButtonClassName = 'resume__icon--minus-button';
 
@@ -15,12 +17,35 @@ const collapseResumeItem = (clickEvent) => {
     let classes = clickEvent.srcElement.classList;
 
     if (classes.contains(plusButtonClassName)) {
+        if (isMobileButton(classes)) {
+            // TODO minimalize access to 4th parent
+            parentElement = parentElement.parentElement.parentElement.parentElement;
+        } 
+
         parentElement.classList.toggle('resume__row--collapsed');
         classes.remove(plusButtonClassName);
         classes.add(minusButtonClassName);
+
     } else if (classes.contains(minusButtonClassName)) {
+        if (isMobileButton(classes)) {
+            parentElement = parentElement.parentElement.parentElement.parentElement;
+        }
+
         parentElement.classList.toggle('resume__row--collapsed');
         classes.remove(minusButtonClassName);
-        classes.add(plusButtonClassName);
+        classes.add(plusButtonClassName); 
     }
- };
+};
+
+const isMobileButton = (buttonClasses) => {
+    var isMobile = false;
+
+    buttonClasses.forEach(function(buttonClass) {
+        console.log(buttonClass);
+        if (buttonClass.includes('--mobile')) {
+            isMobile = true;
+        }
+    });
+
+    return isMobile;
+}
